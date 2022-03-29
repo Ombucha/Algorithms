e94db08079d5c979af517a65b29e35cbf11b3761
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-03-29 16:27:14 +0530

    Add Bubble Sort

---
+++ sort/bubble_sort.py
@@ -0,0 +1,24 @@
+import copy
+
+def bubble_sort_iterative(array: list[float]) -> list[float]:
+    _array = copy.copy(array)
+    length = len(_array)
+    swapped = True
+    while swapped:
+        swapped = False
+        for index in range(length - 1):
+            if _array[index] > _array[index + 1]:
+                _array[index], _array[index + 1] = _array[index + 1], _array[index]
+                swapped = True
+    return _array
+
+def bubble_sort_recursive(array: list[float], length: int = None) -> list[float]:
+    _array = copy.copy(array)
+    if length is None:
+        length = len(_array)
+    if length == 1:
+        return _array
+    for index in range(length - 1):
+        if _array[index] > _array[index + 1]:
+            _array[index], _array[index + 1] = _array[index + 1], _array[index]
+    return bubble_sort_recursive(_array, length - 1)
