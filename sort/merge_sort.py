bb1f6e9bb0945791044539fa31ed46f54e56d7b3
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-03-29 12:53:31 +0530

    Add Merge Sort

---
+++ sort/merge_sort.py
@@ -0,0 +1,16 @@
+import math
+
+def merge(array_1: list[float], array_2: list[float]) -> list[float]:
+    if len(array_1) == 0:
+        return array_2
+    if len(array_2) == 0:
+        return array_1
+    if array_1[0] < array_2[0]:
+        return [array_1[0]] + merge(array_1[1:], array_2)
+    return [array_2[0]] + merge(array_1, array_2[1:])
+
+def merge_sort(array: list[float]) -> list[float]:
+    length = len(array)
+    if length == 1:
+        return array
+    return merge(merge_sort(array[:math.floor(length / 2)]), merge_sort(array[math.floor(length / 2):]))
