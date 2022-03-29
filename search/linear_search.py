875659babf342573c09a32f9fb02e36f4a345803
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-03-29 11:06:06 +0530

    Add Linear Search

---
+++ search/linear_search.py
@@ -0,0 +1,17 @@
+import typing
+
+def linear_search_iterative(array: list, target: typing.Any) -> int | None:
+    for index, element in enumerate(array):
+        if element == target:
+            return index
+    return None
+
+def linear_search_recursive(array: list, target: typing.Any) -> int | None:
+    length = len(array)
+    if length == 1:
+        if array[0] == target:
+            return 0
+        return None
+    if array[length - 1] == target:
+        return length - 1
+    return linear_search_recursive(array[:length - 1], target)
