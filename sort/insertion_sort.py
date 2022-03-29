956edaeeafc7d29183c205e9dd745811ae6372bc
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-03-29 15:22:31 +0530

    Add Recursive Insertion Sort

--- sort/insertion_sort.py
+++ sort/insertion_sort.py
@@ -1,3 +1,5 @@
+import copy
+
 def insertion_sort_iterative(array: list[float]) -> list[float]:
     for index, key in enumerate(array):
         _index = index - 1
@@ -6,3 +8,18 @@ def insertion_sort_iterative(array: list[float]) -> list[float]:
             _index -= 1
         array[_index + 1] = key
     return array
+
+def insertion_sort_recursive(array: list[float], length: int = None) -> list[float]:
+    _array = copy.copy(array)
+    if length is None:
+        length = len(_array)
+    if length == 1:
+        return _array
+    last_element = _array[length - 1]
+    index = length - 2
+    _array = insertion_sort_recursive(_array, length - 1)
+    while index >= 0 and _array[index] > last_element:
+        _array[index + 1] = _array[index]
+        index -= 1
+    _array[index + 1] = last_element
+    return _array
