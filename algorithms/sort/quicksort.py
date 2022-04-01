e22d86bafddba53a9a87db72d354bb33adfc841e
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-01 09:52:30 +0530

    Add Iterative Quicksort

--- algorithms/sort/quicksort.py
+++ algorithms/sort/quicksort.py
@@ -10,6 +10,33 @@ def partition(array: list[float], low: int, high: int) -> int:
     array[_pivot + 1], array[high] = array[high], array[_pivot + 1]
     return _pivot + 1
 
+def quicksort_iterative(array, low, high):
+    _array = copy.copy(array)
+    size = high - low + 1
+    stack = [0] * (size)
+    top = -1
+    top += 1
+    stack[top] = low
+    top += 1
+    stack[top] = high
+    while top >= 0:
+        high = stack[top]
+        top = top - 1
+        low = stack[top]
+        top = top - 1
+        pivot = partition(_array, low, high)
+        if pivot - 1 > low:
+            top = top + 1
+            stack[top] = low
+            top = top + 1
+            stack[top] = pivot - 1
+        if pivot + 1 < high:
+            top = top + 1
+            stack[top] = pivot + 1
+            top = top + 1
+            stack[top] = high
+    return _array
+
 def quicksort_recursive(array: list[float], low: int = 0, high: int = None) -> list[float]:
     _array = copy.copy(array)
     if high is None:
