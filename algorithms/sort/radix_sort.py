3b3bce3a8dbb9aa9ba2c72bfb07fd507ca69cb49
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-01 14:17:04 +0530

    Add Radix Sort

---
+++ algorithms/sort/radix_sort.py
@@ -0,0 +1,27 @@
+import copy
+
+def counting_sort(array: list[float], exponent: int) -> list[float]:
+    length = len(array)
+    output = [0] * (length)
+    count = [0] * (10)
+    for index in range(0, length):
+        _index = int(array[index] // exponent)
+        count[_index % 10] += 1
+    for index in range(1, 10):
+        count[index] += count[index - 1]
+    index = length - 1
+    while index >= 0:
+        _index = int(array[index] // exponent)
+        output[count[_index % 10] - 1] = array[index]
+        count[_index % 10] -= 1
+        index -= 1
+    return output
+
+def radix_sort(array: list[float]) -> list[float]:
+    _array = copy.copy(array)
+    maximum = max(_array)
+    exponent = 1
+    while maximum / exponent > 1:
+        _array = counting_sort(array, exponent)
+        exponent *= 10
+    return _array
