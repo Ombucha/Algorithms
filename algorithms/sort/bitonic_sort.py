8403809c71c5490c7d2d75d49d8513f135958f6b
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-01 15:41:01 +0530

    Fix Bitonic Sort Indentation

--- algorithms/sort/bitonic_sort.py
+++ algorithms/sort/bitonic_sort.py
@@ -1,7 +1,7 @@
 import copy
 import typing
 
-def bitonic_sort_recursive(array: list[float], low: int = 0, length: int = None, direction: typing.Literal[0, 1] = 1) -> list[float]:    
+def bitonic_sort_recursive(array: list[float], low: int = 0, length: int = None, direction: typing.Literal[0, 1] = 1) -> list[float]:
 
     def merge(array: list[float], low: int, length: int, direction: typing.Literal[0, 1]) -> list[float]:
         _array = copy.copy(array)
@@ -18,8 +18,8 @@ def bitonic_sort_recursive(array: list[float], low: int = 0, length: int = None,
         length = len(array)
     _array = copy.copy(array)
     if length > 1:
-          _length = length // 2
-          _array = bitonic_sort_recursive(_array, low, _length, 1)
-          _array = bitonic_sort_recursive(_array, low + _length, _length, 0)
-          _array = merge(_array, low, length, direction)
+        _length = length // 2
+        _array = bitonic_sort_recursive(_array, low, _length, 1)
+        _array = bitonic_sort_recursive(_array, low + _length, _length, 0)
+        _array = merge(_array, low, length, direction)
     return _array
