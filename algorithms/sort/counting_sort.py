d2061c4729839420d45af56acc8a87effc93c9ea
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-03 19:25:27 +0530

    Add Counting Sort

---
+++ algorithms/sort/counting_sort.py
@@ -0,0 +1,14 @@
+def counting_sort(array: list[int], number: int = None) -> list[int]:
+    if number is None:
+        number = len(array)
+    count = (number + 1) * [0]
+    output = len(array) * [0]
+    for value in array:
+        count[value] += 1
+    for index in range(number):
+        count[index + 1] += count[index]
+    for index in range(len(array) - 1, -1, -1):
+        value = array[index]
+        count[value] -= 1
+        output[count[value]] = array[index]
+    return output
