bb6273bb1fc0c4bc2268683b677a15c59e7407c1
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-03 09:38:05 +0530

    Add Bead Sort

---
+++ algorithms/sort/bead_sort.py
@@ -0,0 +1,9 @@
+def bead_sort(array: list[int]) -> list[int]:
+    output = []
+    transposed_array = [0] * max(array)
+    for number in array:
+        transposed_array[:number] = [element + 1 for element in transposed_array[:number]]
+    for _ in array:
+        output.append(sum(n > 0 for n in transposed_array))
+        transposed_array = [n - 1 for n in transposed_array]
+    return output
