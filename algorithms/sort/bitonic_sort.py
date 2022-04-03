71f7a887de9e75b32ecb8efa0262c2052ef6f808
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-03 09:26:09 +0530

    Add Iterative Bitonic Sort

--- algorithms/sort/bitonic_sort.py
+++ algorithms/sort/bitonic_sort.py
@@ -23,3 +23,21 @@ def bitonic_sort_recursive(array: list[float], low: int = 0, length: int = None,
         _array = bitonic_sort_recursive(_array, low + _length, _length, 0)
         _array = merge(_array, low, length, direction)
     return _array
+
+def bitonic_sort_iterative(array: list[float]) -> list[float]:
+    _array = copy.copy(array)
+    length = len(_array) - 1
+    index = 2
+    while index <= length:
+        _index = index / 2
+        while _index > 0:
+            __index = 0
+            while __index < length:
+                value = __index ^ int(_index)
+                if value > __index:
+                    if (((__index & index) == 0) and (_array[__index] > _array[value]) or ((__index & index) != 0) and (_array[__index] < _array[value])):
+                        _array[__index], _array[value] = _array[value], _array[__index]
+                __index += 1
+            _index //= 2
+        index *= 2
+    return _array
