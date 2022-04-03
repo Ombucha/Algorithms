2ddfdbcd37f8e53e4c8d3af1de3b47bdcb02191a
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-03 10:28:20 +0530

    Add `next_sort` to Bucket Sort

--- algorithms/sort/bucket_sort.py
+++ algorithms/sort/bucket_sort.py
@@ -1,5 +1,14 @@
 import math
 
+def next_sort(array: list[float]) -> list[float]:
+    for index, key in enumerate(array):
+        _index = index - 1
+        while _index > -1 and array[_index] > key:
+            array[_index + 1] = array[_index]
+            _index -= 1
+        array[_index + 1] = key
+    return array
+
 def bucket_sort(array: list[float], number: int = None) -> list[float]:
     if number is None:
         number = len(array)
@@ -8,7 +17,7 @@ def bucket_sort(array: list[float], number: int = None) -> list[float]:
     for index in range(len(array)):
         buckets[math.floor((number * array[index]) / maximum) - 1].append(array[index])
     for index in range(number):
-        buckets[index].sort()
+        next_sort(buckets[index])
     output = []
     for index in range(number):
         output += buckets[index]
