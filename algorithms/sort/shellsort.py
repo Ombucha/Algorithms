5feda5abcd2d2b7f99b6c7f26726e1f0a6ccb623
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-04 15:51:26 +0530

    Add Shellsort

---
+++ algorithms/sort/shellsort.py
@@ -0,0 +1,17 @@
+import copy
+
+def shellsort(array: list[float]) -> list[float]:
+    _array = copy.copy(array)
+    gaps = [701, 301, 132, 57, 23, 10, 4, 1]
+    for gap in gaps:
+        for offset in range(gap):
+            index = offset
+            while index < len(_array):
+                __array = _array[index]
+                _index = index
+                while _index >= gap and _array[_index - gap] > __array:
+                    _array[_index] = _array[_index - gap]
+                    _index -= gap
+                _array[_index] = __array
+                index += gap
+    return _array
