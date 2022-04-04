d780ef1eaa73ec495d0490e589cc36807a2456fe
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-04 15:57:09 +0530

    Add Cycle Sort

---
+++ algorithms/sort/cycle_sort.py
@@ -0,0 +1,24 @@
+import copy
+
+def cycle_sort(array: list[float]) -> list[float]:
+    _array = copy.copy(array)
+    for index in range(0, len(_array) - 1):
+        item = _array[index]
+        position = index
+        for _index in range(index + 1, len(_array) - 1):
+            if _array[_index] < item:
+                position += 1
+        if position == index:
+            continue
+        while item == _array[position]:
+            position += 1
+        _array[position], item = item, _array[position]
+        while position != index:
+            position = index
+            for _index in range(index + 1, len(_array) - 1):
+                if _array[_index] < item:
+                    position += 1
+            while item == _array[position]:
+                position += 1
+            _array[position], item = item, _array[position]
+    return _array
