8f34bf30688c46eb2338ef39ea8a2e3ec362814b
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-04 16:23:48 +0530

    Add Slowsort

---
+++ algorithms/sort/slowsort.py
@@ -0,0 +1,15 @@
+import copy
+
+def slowsort(array: list[float], left: int = 0, right: int = None) -> list[float]:
+    if right is None:
+        right = len(array) - 1
+    if left >= right:
+        return array
+    _array = copy.copy(array)
+    middle = (left + right) // 2
+    _array = slowsort(_array, left, middle)
+    _array = slowsort(_array, middle + 1, right)
+    if _array[right] < _array[middle]:
+        _array[right], _array[middle] = _array[middle], _array[right]
+    _array = slowsort(_array, left, right - 1)
+    return _array
