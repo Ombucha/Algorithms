cf9fe1d4b461d6c4ef7e9c06e0ca10863e54918b
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-04 15:34:12 +0530

    Add Stooge Sort

---
+++ algorithms/sort/stooge_sort.py
@@ -0,0 +1,14 @@
+import copy
+
+def stooge_sort(array: list[float], left: int = 0, right: int = None) -> list[float]:
+    _array = copy.copy(array)
+    if right is None:
+        right = len(_array) - 1
+    if _array[left] > _array[right]:
+        _array[right], _array[left] = _array[left], _array[right]
+    if (right - left + 1) > 2:
+        index = (right - left + 1) // 3
+        _array = stooge_sort(_array, left, right - index)
+        _array = stooge_sort(_array, left + index, right)
+        _array = stooge_sort(_array, left, right - index)
+    return _array
