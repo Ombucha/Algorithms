26c5ca9af54214a3bbd577a50136320fee0a79e3
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-04 09:45:06 +0530

    Add Cocktail Shaker Sort

---
+++ algorithms/sort/cocktail_shaker_sort.py
@@ -0,0 +1,19 @@
+import copy
+
+def cocktail_shaker_sort(array: list[float]) -> list[float]:
+    _array = copy.copy(array)
+    swapped = True
+    while swapped:
+        swapped = False
+        for index in range(len(_array) - 1):
+            if _array[index] > _array[index + 1]:
+                _array[index], _array[index + 1] = _array[index + 1], _array[index]
+                swapped = True
+        if not swapped:
+            break
+        swapped = False
+        for index in range(len(_array) - 1):
+            if _array[index] > _array[index + 1]:
+                _array[index], _array[index + 1] = _array[index + 1], _array[index]
+                swapped = True
+    return _array
