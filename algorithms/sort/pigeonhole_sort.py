3e16d809578e129433e76d3baa5635f98136708d
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-04 14:26:17 +0530

    Add Pigeonhole Sort

---
+++ algorithms/sort/pigeonhole_sort.py
@@ -0,0 +1,15 @@
+import copy
+
+def pigeonhole_sort(array: list[float]) -> list[float]:
+    _array = copy.copy(array)
+    base = min(key for key in _array)
+    size = max(key for key in _array) - base + 1
+    pigeonholes = [[] for _ in range(size)]
+    for key in _array:
+        index = key - base
+        pigeonhole = pigeonholes[index]
+        pigeonhole.append(key)
+    _array.clear()
+    for pigeonhole in pigeonholes:
+        _array.extend(pigeonhole)
+    return _array
