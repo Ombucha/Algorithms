3175c8e375753b2d265f2dd6166f320d01b6845c
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-05 13:07:02 +0530

    Change `sorted` to `is_sorted`

--- algorithms/sort/odd_even_sort.py
+++ algorithms/sort/odd_even_sort.py
@@ -2,15 +2,15 @@ import copy
 
 def odd_even_sort(array: list[float]):
     _array = copy.copy(array)
-    sorted = False
-    while not sorted:
-        sorted = True
+    is_sorted = False
+    while not is_sorted:
+        is_sorted = True
         for index in range(1, len(_array) - 1, 2):
             if _array[index] > _array[index + 1]:
                 _array[index], _array[index + 1] = _array[index + 1], _array[index]
-                sorted = False
+                is_sorted = False
         for index in range(0, len(_array) - 1, 2):
             if _array[index] > _array[index + 1]:
                 _array[index], _array[index + 1] = _array[index + 1], _array[index]
-                sorted = False
+                is_sorted = False
     return _array
