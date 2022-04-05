f969f67845b4c4fa132eb6df76427a61d6a91c94
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-05 15:25:49 +0530

    Improve Bucket Sort

--- algorithms/sort/bucket_sort.py
+++ algorithms/sort/bucket_sort.py
@@ -1,13 +1,14 @@
-def next_sort(array: list[float]) -> list[float]:
-    for index, key in enumerate(array):
-        _index = index - 1
-        while _index > -1 and array[_index] > key:
-            array[_index + 1] = array[_index]
-            _index -= 1
-        array[_index + 1] = key
-    return array
-
 def bucket_sort(array: list[float], number: int = None) -> list[float]:
+
+    def next_sort(array: list[float]) -> list[float]:
+        for index, key in enumerate(array):
+            _index = index - 1
+            while _index > -1 and array[_index] > key:
+                array[_index + 1] = array[_index]
+                _index -= 1
+            array[_index + 1] = key
+        return array
+
     if number is None:
         number = len(array)
     buckets = [[] for _ in range(number)]
