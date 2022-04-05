fc72c46810134f656907c15dc83b4cb70a0f8ee4
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-05 20:58:07 +0530

    Add Recursive Collatz Sequence

--- algorithms/math/sequence/collatz_sequence.py
+++ algorithms/math/sequence/collatz_sequence.py
@@ -1,4 +1,4 @@
-def collatz_sequence(number: int) -> int:
+def collatz_sequence_iterative(number: int) -> int:
     sequence = [number]
     term = number
     while term != 1:
@@ -7,4 +7,16 @@ def collatz_sequence(number: int) -> int:
         else:
             term = 3 * term + 1
         sequence.append(term)
+    return sequence
+
+def collatz_sequence_recursive(number: int) -> int:
+    if number == 1:
+        return [1]
+    sequence = [number]
+    term = number
+    if term % 2 == 0:
+        term //= 2
+    else:
+        term = 3 * term + 1
+    sequence += collatz_sequence_recursive(term)
     return sequence
