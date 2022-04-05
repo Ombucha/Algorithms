56a7d4c3fcd385ba72eae64017aed0ac2a2df371
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-05 15:44:39 +0530

    Add Jump Search

---
+++ algorithms/search/jump_search.py
@@ -0,0 +1,18 @@
+import math
+
+def jump_search(array: list[float], target: float) -> int:
+    length = len(array)
+    index = 0
+    _index = math.floor(math.sqrt(length))
+    while array[min(_index, length) - 1] < target:
+        index = _index
+        _index += math.floor(math.sqrt(length))
+        if index >= length:
+            return None
+    while array[index] < target:
+        index += 1
+        if index == min(_index, length):
+            return None
+    if array[index] == target:
+        return index
+    return None
