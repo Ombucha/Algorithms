78f43ade1db76fd87eadf083e793409b5583b3a6
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-05 15:20:43 +0530

    Improve Heapsort

--- algorithms/sort/heapsort.py
+++ algorithms/sort/heapsort.py
@@ -1,18 +1,19 @@
 import copy
 
-def heapify(array: list[float], length: int, index: int) -> None:
-    largest = index
-    left = 2 * index + 1
-    right = 2 * index + 2
-    if left < length and array[largest] < array[left]:
-        largest = left 
-    if right < length and array[largest] < array[right]:
-        largest = right
-    if largest != index:
-        array[index], array[largest] = array[largest], array[index]
-        heapify(array, length, largest)
- 
 def heapsort(array: list[float]) -> list[float]:
+
+    def heapify(array: list[float], length: int, index: int) -> None:
+        largest = index
+        left = 2 * index + 1
+        right = 2 * index + 2
+        if left < length and array[largest] < array[left]:
+            largest = left
+        if right < length and array[largest] < array[right]:
+            largest = right
+        if largest != index:
+            array[index], array[largest] = array[largest], array[index]
+            heapify(array, length, largest)
+
     _array = copy.copy(array)
     length = len(_array)
     for index in range(length // 2 - 1, -1, -1):
