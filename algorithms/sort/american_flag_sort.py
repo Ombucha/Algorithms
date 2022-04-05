189f37def3594a6cafd420b9a5bc7fd07be100af
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-05 12:36:43 +0530

    Add American Flag Sort

---
+++ algorithms/sort/american_flag_sort.py
@@ -0,0 +1,51 @@
+import copy
+import math
+
+def american_flag_sort(array: list[int], radix: int = None) -> list[int]:
+
+    def get_radix_value(number: int, digit: int, radix: int) -> int:
+        return int(math.floor(number / radix ** digit)) % radix
+
+    def compute_offsets(array: list[int], start: int, end: int, digit: int, radix: int) -> list[int]:
+        counts = [0 for _ in range(radix)]
+        for index in range(start, end):
+            value = get_radix_value(array[index], digit, radix)
+            counts[value] += 1
+        offsets = [0 for _ in range(radix)]
+        sum = 0
+        for index in range(radix):
+            offsets[index] = sum
+            sum += counts[index]
+        return offsets
+
+    def swap(array: list[int], offsets, start: int, end: int, digit: int, radix: int) -> None:
+        index = start
+        next_free = copy.copy(offsets)
+        block = 0
+        while block < radix - 1:
+            if index >= start + offsets[block + 1]:
+                block += 1
+                continue
+            value = get_radix_value(array[index], digit, radix)
+            if value == block:
+                index += 1
+                continue
+            swap_to = start + next_free[value]
+            array[index], array[swap_to] = array[swap_to], array[index]
+            next_free[value] += 1
+
+    def american_flag_sort_helper(array: list[int], start: int, end: int, digit: int, radix: int) -> None:
+        offsets = compute_offsets(array, start, end, digit, radix)
+        swap(array, offsets, start, end, digit, radix)
+        if digit == 0:
+            return
+        for index in range(len(offsets) - 1):
+            american_flag_sort_helper(array, offsets[index], offsets[index + 1], digit - 1, radix)
+
+    if radix is None:
+        radix = len(array)
+    _array = copy.copy(array)
+    maximum_value = max(_array)
+    maximum_digit = int(math.floor(math.log(maximum_value, radix)))
+    american_flag_sort_helper(_array, 0, len(_array), maximum_digit, radix)
+    return _array
