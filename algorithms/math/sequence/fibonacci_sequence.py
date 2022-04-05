c2029511735b559977563c251548abac7362cd82
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-05 21:05:56 +0530

    Add Fibonacci Sequence

---
+++ algorithms/math/sequence/fibonacci_sequence.py
@@ -0,0 +1,15 @@
+def fibonacci_sequence_iterative(number: int) -> list[int]:
+    sequence = [0, 1]
+    index = 2
+    while index <= number:
+        term = sequence[index - 1] + sequence[index - 2]
+        sequence.append(term)
+        index += 1
+    return sequence
+
+def fibonacci_sequence_recursive(number: int) -> list[int]:
+    if number == 1:
+        return [0, 1]
+    sequence = fibonacci_sequence_recursive(number - 1)
+    sequence += [sequence[-1] + sequence[-2]]
+    return sequence
