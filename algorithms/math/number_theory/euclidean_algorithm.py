1ce06bea559d93e46a024549c51dc4040ae70c24
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-06 09:30:53 +0530

    Add Euclidean Algorithm

---
+++ algorithms/math/number_theory/euclidean_algorithm.py
@@ -0,0 +1,13 @@
+def euclidean_algorithm_recursive(number_1: int, number_2: int) -> int:
+    while number_1 != number_2: 
+        if number_1 > number_2:
+            number_1 = number_1 - number_2
+        else:
+            number_2 = number_2 - number_1
+    return number_1
+
+def euclidean_algorithm_recursive(number_1: int, number_2: int) -> int:
+    if number_2 == 0:
+        return number_1
+    else:
+        return euclidean_algorithm_recursive(number_2, number_1 % number_2)
