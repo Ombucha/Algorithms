fdabf8f0b545b66d436f384bfb94fc0552a23ce7
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-06 13:12:59 +0530

    Remove Trailing Whitespace

--- algorithms/math/number_theory/trial_division.py
+++ algorithms/math/number_theory/trial_division.py
@@ -2,7 +2,7 @@ def trial_division(number: int) -> list[int]:
     factors = []
     factor = 2
     while number > 1:
-        if number % factor == 0:    
+        if number % factor == 0:
             factors.append(factor)
             number //= factor
         else:
