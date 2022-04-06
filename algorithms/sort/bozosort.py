bf2258a3c76ea6fe87b005e94944ab418550b209
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-06 15:21:43 +0530

    Improve Bozosort

--- algorithms/sort/bozosort.py
+++ algorithms/sort/bozosort.py
@@ -1,11 +1,12 @@
 import copy
 import random
 
-def is_sorted(array: list[float]) -> bool:
-    boolean = all(index <= _index for index, _index in zip(array, array[1:]))
-    return boolean
-
 def bozosort(array: list[float]) -> list[float]:
+
+    def is_sorted(array: list[float]) -> bool:
+        boolean = all(index <= _index for index, _index in zip(array, array[1:]))
+        return boolean
+
     _array = copy.copy(array)
     while not is_sorted(_array):
         elements = random.sample(range(len(_array)), k = 2)
