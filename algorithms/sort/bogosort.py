4e09ec0077c22a7505e3d9eb314c81cde02fcad1
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-06 15:22:19 +0530

    Improve Bogosort

--- algorithms/sort/bogosort.py
+++ algorithms/sort/bogosort.py
@@ -1,11 +1,12 @@
 import copy
 import random
 
-def is_sorted(array: list[float]) -> bool:
-    boolean = all(index <= _index for index, _index in zip(array, array[1:]))
-    return boolean
-
 def bogosort(array: list[float]) -> list[float]:
+
+    def is_sorted(array: list[float]) -> bool:
+        boolean = all(index <= _index for index, _index in zip(array, array[1:]))
+        return boolean
+
     _array = copy.copy(array)
     while not is_sorted(_array):
         random.shuffle(_array)
