62a531c9b43b5487d31ac5cfb7c820618a9fe49f
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-06 12:20:45 +0530

    Add Wilson's Primality Test

---
+++ algorithms/math/number_theory/wilson_primality_test.py
@@ -0,0 +1,11 @@
+import factorial
+
+def wilson_primality_test(number: int) -> int:
+    if number in (0, 1, 4):
+        return False
+    if number in (2, 3):
+        return True
+    test = (factorial.factorial_iterative(number - 1) + 1) % number
+    if test == 0:
+        return True
+    return False
