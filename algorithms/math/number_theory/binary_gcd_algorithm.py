668e6b05803b849f9ce514f72e2022fef1b09398
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-06 15:31:23 +0530

    Add Binary GCD Algorithm

---
+++ algorithms/math/number_theory/binary_gcd_algorithm.py
@@ -0,0 +1,19 @@
+def binary_gcd_algorithm(number_1: int, number_2: int) -> int:
+    if (number_1 == 0):
+        return number_2
+    if (number_2 == 0):
+        return number_1
+    index = 0
+    while (number_1 | number_2) & 1 == 0:
+        number_1 >>= 1
+        number_2 >>= 1
+        index += 1
+    while (number_1 & 1) == 0:
+        number_1 >>= 1
+    while number_2 != 0:
+        while number_2 & 1 == 0:
+            number_2 >>= 1 
+        if number_1 > number_2:
+            number_1, number_2 = number_2, number_1
+        number_2 -= number_1
+    return number_1 << index
