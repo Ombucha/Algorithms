2b497135b8a66a78c5d349ff75530aa573bacb11
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-06 16:04:59 +0530

    Add Euler's Totient Function

---
+++ algorithms/math/number_theory/euler_totient_function.py
@@ -0,0 +1,8 @@
+import binary_gcd_algorithm
+
+def euler_totient_function(number: int) -> int:
+    coprimes = []
+    for index in range(1, number + 1):
+        if binary_gcd_algorithm.binary_gcd_algorithm(number, index) == 1:
+            coprimes.append(index)
+    return len(coprimes)
