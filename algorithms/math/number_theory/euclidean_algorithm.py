811c03336241fac749870edb7e0e8589f9dc3c1f
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-06 09:47:50 +0530

    Improve Euclidean Algorithm

--- algorithms/math/number_theory/euclidean_algorithm.py
+++ algorithms/math/number_theory/euclidean_algorithm.py
@@ -1,5 +1,5 @@
-def euclidean_algorithm_recursive(number_1: int, number_2: int) -> int:
-    while number_1 != number_2: 
+def euclidean_algorithm_iterative(number_1: int, number_2: int) -> int:
+    while number_1 != number_2:
         if number_1 > number_2:
             number_1 = number_1 - number_2
         else:
@@ -9,5 +9,4 @@ def euclidean_algorithm_recursive(number_1: int, number_2: int) -> int:
 def euclidean_algorithm_recursive(number_1: int, number_2: int) -> int:
     if number_2 == 0:
         return number_1
-    else:
-        return euclidean_algorithm_recursive(number_2, number_1 % number_2)
+    return euclidean_algorithm_recursive(number_2, number_1 % number_2)
