3266a0f466049c39d04ea0e83b7ce196e7137023
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-07 09:44:00 +0530

    Improve Fermat Factorisation

--- algorithms/math/number_theory/fermat_factorisation.py
+++ algorithms/math/number_theory/fermat_factorisation.py
@@ -6,5 +6,5 @@ def fermat_factorisation(number: int) -> int:
     while math.sqrt(__number) % 1 != 0:
         _number += 1
         __number = _number ** 2 - number
-    square_root = int(_number - math.sqrt(__number))
-    return square_root
+    factors = (int(_number - math.sqrt(__number)), int(_number + math.sqrt(__number)))
+    return factors
