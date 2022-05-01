b3cf4becdef07b19e3383733d8192c44477d4fc7
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-05-01 19:06:06 +0530

    Improve Activity Selection

--- algorithms/greedy_algorithms/activity_selection.py
+++ algorithms/greedy_algorithms/activity_selection.py
@@ -3,7 +3,7 @@ import typing
 
 class Activity:
 
-    def __init__(self, start_time, finish_time) -> None:
+    def __init__(self, start_time: float, finish_time: float) -> None:
         self.start_time = start_time
         self.finish_time = finish_time
 
@@ -12,15 +12,13 @@ class Activity:
             yield item
 
 def activity_selection(activities: list[Activity]) -> list[Activity]:
-    start_times = [activity.start_time for activity in activities]
-    finish_times = [activity.finish_time for activity in activities]
     _activities = copy.copy(activities)
     _activities.sort(key = lambda activity: activity.finish_time)
     selected = {_activities[0]}
-    index = 1
+    latest = _activities[0].finish_time
     length = len(_activities)
-    for _index in range(1, length):
-        if start_times[_index] >= finish_times[index]:
-            selected = selected.union({_activities[_index]})
-            index = _index
+    for index in range(1, length):
+        if _activities[index].start_time >= latest:
+            selected.add(_activities[index])
+            latest = _activities[index].finish_time
     return selected
