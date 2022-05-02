ae2ef9700b24b443962b47452570ee252dcb7b32
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-05-02 19:53:54 +0530

    Add Prim's Algorithm
    The objective is to find a `Minimum Spanning Tree`.

---
+++ algorithms/graphs/prim_algorithm.py
@@ -0,0 +1,15 @@
+import networkx
+
+def prim_algorithm(graph: networkx.Graph, root: str | float) -> networkx.Graph:
+    tree = networkx.Graph()
+    vertices = set(graph.nodes)
+    explored = {root}
+    while explored != vertices:
+        edges = {}
+        for edge in set(graph.edges):
+            if (edge[0] in explored and edge[1] in vertices.difference(explored)) or (edge[1] in explored and edge[0] in vertices.difference(explored)):
+                edges[edge] = graph.get_edge_data(edge[0], edge[1])["weight"]
+        edge = min(edges, key = edges.get)
+        tree.add_edge(edge[0], edge[1])
+        explored = explored.union(set(edge))
+    return tree
