13296f9664d4c89f70b13afcf41d4a5461966705
Omkaar <omkaar.nerurkar@gmail.com>
2022-05-07 09:19:56 +0530

    Improve the `number_theory` Folder Structure

---
+++ algorithms/math/number_theory/primality_tests/lucas_primality_test.py
@@ -0,0 +1,16 @@
+import random
+import sympy
+
+def lucas_primality_test(number: int, accuracy: int = None) -> bool:
+    if accuracy is None:
+        accuracy = number
+    factors = sympy.primefactors(number - 1)
+    for _ in range(accuracy):
+        integer = random.randint(2, number - 1)
+        if (integer ** (number - 1)) % number != 1:
+            return False
+        for index, factor in enumerate(factors):
+            if (integer ** ((number - 1) // factor)) % number != 1:
+                if index == len(factors) - 2:
+                    return True
+    return False
